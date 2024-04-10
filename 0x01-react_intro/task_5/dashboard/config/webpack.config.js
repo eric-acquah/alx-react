46a408a2ffb5cc6c500dfdcc32119e8577ab276f
Eric Acquah <ericacquah1720@gmail.com>
2024-04-10 16:41:20 +0000

    React ft. Webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+  devServer: {
+    static: 'dist',
+    hot: true,
+  },
+};
