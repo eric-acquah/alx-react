0c3e6ab2b0d7e1d366771ec7fff0bb17e9e6e670
Eric Acquah <ericacquah1720@gmail.com>
2024-04-10 16:34:16 +0000

    Modified Notification

---
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+export function getFullYear() {
+  const d = new Date();
+
+  return d.getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  if (isIndex) return "Holberton School";
+
+  return "Holberton School main dashboard";
+}
+
+export function getLatestNotification() {
+  return (
+   "<strong>Urgent requirement</strong> - complete by EOD"
+  )
+}
